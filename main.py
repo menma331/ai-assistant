374605927c31576ac27bda4a711818a5604492e6
roman <romaxid@yandex.by>
2025-03-08 14:39:05 +0300

    запускатор

---
+++ main.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from settings import settings
+from handlers.start import start_router
+from handlers.ai import ai_router
+
+bot = Bot(token=settings.tg_bot_token)
+dp = Dispatcher()
+
+
+
+async def start():
+    logging.basicConfig(level=logging.INFO)  # Чтобы видеть, как бот обрабатывает сообщения
+
+    # Подключаем роутеры
+    dp.include_router(start_router)
+    dp.include_router(ai_router)
+
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(start())
