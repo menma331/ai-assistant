c8c463af5bafda7bf453d7fb96f2e1d055b1ae56
roman <romaxid@yandex.by>
2025-03-13 16:44:05 +0300

    добавлена новая переменная

--- core/settings.py
+++ core/settings.py
@@ -4,9 +4,11 @@ from pydantic import Field
 
 class Settings(BaseSettings):
     tg_bot_token: str = Field(alias="TELEGRAM_BOT_TOKEN", description="Токен телеграм бота(обязателен)")
+
     open_ai_token: str = Field(alias="OPEN_AI_TOKEN", description="Токен OpenAI(обязателен)")
     open_ai_assistant_id: str = Field(alias="OPEN_AI_ASSISTANT_ID", description="ID созданного ассистента. Может создаться кодом, либо в панели управления "
                                                                                 "OpenAI")
+    amplitude_api_key: str = Field(alias="AMPLITUDE_API_KEY", description="АПИ ключ от Amplitude")
 
     database_connection_string: str = Field(alias="DB_CONNECTION_STRING", description="Строка подключения к базе данных в формате DSN")
 
