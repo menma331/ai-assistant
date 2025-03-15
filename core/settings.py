fb55d88cde5af027a639d05355c27e5098763852
roman <romaxid@yandex.by>
2025-03-15 16:33:41 +0300

    добавлена загрузка переменных окружения. В настройки добавленых новые переменные для файлов и редиса

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,9 @@
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
 
+
+load_dotenv()
 
 class Settings(BaseSettings):
     tg_bot_token: str = Field(alias="TELEGRAM_BOT_TOKEN", description="Токен телеграм бота(обязателен)")
@@ -8,10 +11,14 @@ class Settings(BaseSettings):
     open_ai_token: str = Field(alias="OPEN_AI_TOKEN", description="Токен OpenAI(обязателен)")
     open_ai_assistant_id: str = Field(alias="OPEN_AI_ASSISTANT_ID", description="ID созданного ассистента. Может создаться кодом, либо в панели управления "
                                                                                 "OpenAI")
+    vector_store_id:str=Field(alias="VECTOR_STORE_ID", description="Vector store ID")
     amplitude_api_key: str = Field(alias="AMPLITUDE_API_KEY", description="АПИ ключ от Amplitude")
 
     database_connection_string: str = Field(alias="DB_CONNECTION_STRING", description="Строка подключения к базе данных в формате DSN")
 
-    model_config = SettingsConfigDict(env_file='./.env')
+    redis_host: str = Field(alias="REDIS_HOST", description="Redis host")
+    redis_port: int = Field(alias="REDIS_PORT", description="Redis port")
+
+    model_config = SettingsConfigDict(env_file='./.env', env_file_encoding="utf-8")
 
 settings = Settings()
